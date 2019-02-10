a7bfa16b21195283c822d6f12fa255a382281649
Andy Chan <tkchanat@connect.ust.hk>
2019-02-10 17:20:25 +0100

    Change character movement

--- Script/Character.gd
+++ Script/Character.gd
@@ -1,28 +1,17 @@
 extends Spatial
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
-
 # Public properties
 export var speed = 10.0
-export var sensitivity = 10.0
+export var sensitivity = 1.0
 
 # Private properties
 var animator
 var camera
 
-var rightDir = Vector3(1.0, 0.0, 0.0)
-var upDir = Vector3(0.0, 1.0, 0.0)
-var forwardDir = Vector3(0.0, 0.0, 1.0)
-
 var lastMousePos = Vector2(0.0, 0.0)
 
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
 	animator = get_node("AnimationPlayer")
 	animator.play("default")
 	animator.playback_speed = 1
@@ -32,31 +21,29 @@ func _ready():
 	
 
 func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-
+	
 	var AnimationState = "default"
 	
 	# Charater movement
 	if Input.is_action_pressed("ui_up"):
-		transform.origin += forwardDir * delta * speed
+		transform = transform.translated(transform.basis.z * delta * speed)
+		#transform.origin += transform.basis * delta * speed
 		AnimationState = "Running"
 	if Input.is_action_pressed("ui_down"):
-		transform.origin -= forwardDir * delta * speed
+		transform = transform.translated(-transform.basis.z * delta * speed)
 		AnimationState = "Running"
 	if Input.is_action_pressed("ui_left"):
-		transform.origin += rightDir * delta * speed
+		transform = transform.translated(transform.basis.x * delta * speed)
 	if Input.is_action_pressed("ui_right"):
-		transform.origin -= rightDir * delta * speed	
+		transform = transform.translated(-transform.basis.x * delta * speed)
 
 	# Charater rotation
-	var currentMousePos = get_viewport().get_mouse_position()
-	if currentMousePos.x != lastMousePos.x:
-		var newRotation = Vector3(get_rotation().x, deg2rad(sensitivity), get_rotation().z)
-		set_rotation(newRotation)
+	if currentMousePos != lastMousePos:
+		var newRotation = get_rotation()
+		var pitch = deg2rad(-Input.get_last_mouse_speed().y * delta * sensitivity)
+		var yaw = deg2rad(-Input.get_last_mouse_speed().x * delta * sensitivity)
+		transform.basis = transform.basis.rotated(transform.basis.y, yaw)
 		pass
-		#transform.rotate_object_local(Vector3(1, 0, 0), PI)
-#		transform.basis = transform.basis.rotated(Vector3(1, 0, 0), PI)
 	lastMousePos = currentMousePos
 	
 	if animator.get_current_animation() != AnimationState:
