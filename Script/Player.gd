1a8ba9dfb808c6c148e49e0ed19b56623719c4a0
Andy Chan <tkchanat@connect.ust.hk>
2019-02-10 16:07:19 +0100

    Update references

--- Script/Player.gd
+++ Script/Player.gd
@@ -7,6 +7,8 @@ export var speed = 10.0
 var t
 var camera
 
+var lastMousePos = Vector2(0.0, 0.0)
+
 var rightDir = Vector3(1.0, 0.0, 0.0)
 var upDir = Vector3(0.0, 1.0, 0.0)
 var forwardDir = Vector3(0.0, 0.0, 1.0)
@@ -14,6 +16,7 @@ var forwardDir = Vector3(0.0, 0.0, 1.0)
 func _ready():
 	t = get_transform()
 	camera = get_child(0)
+	lastMousePos = get_viewport().get_mouse_position()
 	pass
 
 func _process(delta):
@@ -28,8 +31,11 @@ func _process(delta):
 		t.origin -= rightDir * delta * speed
 	if Input.is_action_pressed("ui_right"):
 		t.origin += rightDir * delta * speed	
-	set_transform(t)
 	
 	# Charater rotation
+	#var currentMousePos = get_viewport().get_mouse_position()
+	#if currentMousePos != lastMousePos:
+	#	t.rotated(upDir, PI/12)
 	
+	set_transform(t)
 	pass
